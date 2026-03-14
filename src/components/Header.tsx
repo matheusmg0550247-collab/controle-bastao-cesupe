d58ea45a38b183cc4dcdca8cbb2f42a701fec2ae
Matheus <matheusmg0550247@gmail.com>
2026-03-14 16:30:44 -0300

    Fix Header iframe

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { useBastaoStore } from '../store/useBastaoStore'
 import { USUARIOS_SISTEMA } from '../constants'
 
